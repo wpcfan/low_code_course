2e9e3a0f4ae7f844bd240a9a326b8419c1b36bf9
Peng Wang <wpcfan@gmail.com>
2023-07-31 15:22:25 +0800

    4-34 通过重构理解在组件树中共享状态的机制

--- chap04/frontend/home/lib/home_view.dart
+++ chap04/frontend/home/lib/home_view.dart
@@ -1,11 +1,10 @@
 import 'package:common/common.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home/widgets/widgets.dart';
 import 'package:models/models.dart';
 import 'package:page_block_widgets/page_block_widgets.dart';
 import 'package:repositories/repositories.dart';
-import 'package:sliver_tools/sliver_tools.dart';
 
 import 'blocs/blocs.dart';
 
@@ -14,10 +13,6 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const baseScreenWidth = 375.0;
-    final screenWidth = MediaQuery.of(context).size.width;
-    final ratio = screenWidth / baseScreenWidth;
-
     return Scaffold(
       body: BlocProvider(
         create: (context) => HomeBloc(
@@ -45,60 +40,9 @@ class HomeView extends StatelessWidget {
               case FetchStatus.loading:
                 return const Center(child: CircularProgressIndicator());
               default:
-                final blocks = state.blocks;
-                final waterfallItems = state.waterfallItems;
-                final widgets = blocks.map((e) {
-                  if (e.type == PageBlockType.imageRow) {
-                    return SliverToBoxAdapter(
-                      child: ImageRowWidget(
-                        items: e.data.map((e) => e as ImageData).toList(),
-                        config: e.config.withRatio(ratio),
-                        onTap: (value) {
-                          debugPrint('onTap: $value');
-                        },
-                      ),
-                    );
-                  } else if (e.type == PageBlockType.banner) {
-                    return SliverToBoxAdapter(
-                      child: BannerWidget(
-                        items: e.data.map((e) => e as ImageData).toList(),
-                        config: e.config.withRatio(ratio),
-                        onTap: (value) {
-                          debugPrint('onTap: $value');
-                        },
-                      ),
-                    );
-                  } else if (e.type == PageBlockType.productRow) {
-                    return SliverToBoxAdapter(
-                      child: ProductRowWidget(
-                        items: e.data.map((e) => e as Product).toList(),
-                        config: e.config.withRatio(ratio),
-                        onTap: (value) {
-                          debugPrint('onTap: $value');
-                        },
-                        addToCart: (value) => debugPrint('addToCart: $value'),
-                      ),
-                    );
-                  } else if (e.type == PageBlockType.waterfall) {
-                    return WaterfallWidget(
-                      items: waterfallItems,
-                      config: e.config.withRatio(ratio),
-                      onTap: (value) {
-                        debugPrint('onTap: $value');
-                      },
-                      addToCart: (value) => debugPrint('addToCart: $value'),
-                    );
-                  } else {
-                    return Container();
-                  }
-                }).toList();
                 return MyCustomScrollView(
-                  loadMoreWidget: state.isLoadingMore
-                      ? const CupertinoActivityIndicator()
-                      : null,
-                  sliver: MultiSliver(
-                    children: widgets,
-                  ),
+                  loadMoreWidget: const LoadMoreWidget(),
+                  sliver: const SliverListBodyWidget(),
                   onRefresh: () async {
                     context.read<HomeBloc>().add(const HomeRefreshEvent());
                     await context.read<HomeBloc>().stream.firstWhere((state) =>
---
+++ chap04/frontend/home/lib/widgets/load_more_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home/blocs/blocs.dart';
+import 'package:models/models.dart';
+
+class LoadMoreWidget extends StatelessWidget {
+  const LoadMoreWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        switch (state.status) {
+          case FetchStatus.loadingMore:
+            return const Center(child: CircularProgressIndicator());
+          default:
+            return const SizedBox();
+        }
+      },
+    );
+  }
+}
