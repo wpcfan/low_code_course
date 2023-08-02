26c2ae9b9c16c98dacade3482990e78a32428ed8
Peng <wpcfan@gmail.com>
2023-08-02 17:15:45 +0800

    5-4 生成图片，指定响应的 Content Type

--- chap05/backend/src/main/java/com/mooc/backend/rest/ImageController.java
+++ chap05/backend/src/main/java/com/mooc/backend/rest/ImageController.java
@@ -1,16 +1,41 @@
 package com.mooc.backend.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/image")
 public class ImageController {
 
     @GetMapping("/{width}")
-    public String generateImage(@PathVariable int width) {
-        return "Image width: " + width + "px";
+    public ResponseEntity<byte[]> generateImage(@PathVariable int width) throws IOException {
+        return buildImage(width);
+    }
+
+    private ResponseEntity<byte[]> buildImage(int width) throws IOException {
+        BufferedImage image = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, width, width);
+        graphics.setColor(Color.BLACK);
+        graphics.drawString("Hello World!", 10, 10);
+        graphics.dispose();
+        // 创建一个字节数组输出流
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // 将图片写入到字节数组输出流中
+        ImageIO.write(image, "png", baos);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(baos.toByteArray());
     }
 }
