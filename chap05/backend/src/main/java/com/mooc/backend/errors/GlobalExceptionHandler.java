c5ba71878e55b021e53bda390fbf0438a24447a6
Peng <wpcfan@gmail.com>
2023-08-06 11:45:16 +0800

    5-15 定制错误结构：从 WebRequest 中 获取访问者的 User-Agent 和 Locale 信息

--- chap05/backend/src/main/java/com/mooc/backend/errors/GlobalExceptionHandler.java
+++ chap05/backend/src/main/java/com/mooc/backend/errors/GlobalExceptionHandler.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.net.URI;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -27,8 +30,11 @@ public class GlobalExceptionHandler {
      * @param e ConstraintViolationException 是 Jakarta Validation 的异常
      * @return ProblemDetail
      */
-    @ExceptionHandler
-    public ProblemDetail handleConstraintViolationException(ConstraintViolationException e) {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ProblemDetail handleConstraintViolationException(
+            ConstraintViolationException e,
+            WebRequest request
+    ) {
         ProblemDetail body = ProblemDetail
                 .forStatusAndDetail(HttpStatusCode.valueOf(400), "参数错误");
         body.setType(URI.create(hostname + "/errors/constraint-violation-exception"));
@@ -38,6 +44,41 @@ public class GlobalExceptionHandler {
         body.setTitle(title);
         body.setDetail(e.getMessage());
         body.setProperty("hostname", hostname);
+        body.setProperty("user-agent", request.getHeader("User-Agent"));
+        body.setProperty("locale", request.getLocale().toString());
+        return body;
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e,
+            WebRequest request
+    ) {
+        ProblemDetail body = ProblemDetail
+                .forStatusAndDetail(HttpStatusCode.valueOf(400), "参数类型转换错误");
+        body.setType(URI.create(hostname + "/errors/method-argument-type-mismatch-exception"));
+        String title = "参数 " + e.getName() + " 类型应为 " + Objects.requireNonNull(e.getRequiredType()).getSimpleName() + "，但实际值为 " + e.getValue();
+        body.setTitle(title);
+        body.setDetail(e.getMessage());
+        body.setProperty("hostname", hostname);
+        body.setProperty("user-agent", request.getHeader("User-Agent"));
+        body.setProperty("locale", request.getLocale().toString());
+        return body;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(
+            Exception e,
+            WebRequest request
+    ) {
+        ProblemDetail body = ProblemDetail
+                .forStatusAndDetail(HttpStatusCode.valueOf(500), "服务器内部错误");
+        body.setType(URI.create(hostname + "/errors/exception"));
+        body.setTitle(e.getClass().getSimpleName());
+        body.setDetail(e.getMessage());
+        body.setProperty("hostname", hostname);
+        body.setProperty("user-agent", request.getHeader("User-Agent"));
+        body.setProperty("locale", request.getLocale().toString());
         return body;
     }
 
