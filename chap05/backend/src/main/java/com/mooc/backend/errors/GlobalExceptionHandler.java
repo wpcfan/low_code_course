fc043777e7a6b4c639e480de71fa0c1868ded70f
Peng <wpcfan@gmail.com>
2023-08-06 10:56:55 +0800

    5-14 ProblemDetial 的数据结构和自定义字段

--- chap05/backend/src/main/java/com/mooc/backend/errors/GlobalExceptionHandler.java
+++ chap05/backend/src/main/java/com/mooc/backend/errors/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.mooc.backend.errors;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @Value("${hostname}")
+    String hostname;
 
     /**
      * 用于处理 ConstraintViolationException 异常
@@ -24,6 +31,13 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleConstraintViolationException(ConstraintViolationException e) {
         ProblemDetail body = ProblemDetail
                 .forStatusAndDetail(HttpStatusCode.valueOf(400), "参数错误");
+        body.setType(URI.create(hostname + "/errors/constraint-violation-exception"));
+        String title = e.getConstraintViolations().stream()
+                .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
+                .collect(Collectors.joining(";"));
+        body.setTitle(title);
+        body.setDetail(e.getMessage());
+        body.setProperty("hostname", hostname);
         return body;
     }
 
