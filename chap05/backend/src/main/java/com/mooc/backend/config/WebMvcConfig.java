fd428a9f7b6ad97920d636c4d7213304c986d780
Peng <wpcfan@gmail.com>
2023-08-10 14:36:15 +0800

    5-33 实现 Spring Boot 的跨域支持

--- chap05/backend/src/main/java/com/mooc/backend/config/WebMvcConfig.java
+++ chap05/backend/src/main/java/com/mooc/backend/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.mooc.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
