bba3d35b98cedd4972210cd37f7cb85383a32235
Peng <wpcfan@gmail.com>
2023-08-29 19:34:19 +0800

    missing exporting pageblockdata

--- chap06/frontend/models/lib/models.dart
+++ chap06/frontend/models/lib/models.dart
@@ -6,6 +6,7 @@ export 'file_vm.dart';
 export 'image_data.dart';
 export 'my_link.dart';
 export 'page_block.dart';
+export 'page_block_data.dart';
 export 'page_config.dart';
 export 'page_layout.dart';
 export 'problem.dart';
