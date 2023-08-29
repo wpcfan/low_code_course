1937b278fc0b3bf7275e1b40ed3af95e50a715ec
Peng <wpcfan@gmail.com>
2023-08-29 19:10:42 +0800

    fix missing home

--- chap06/frontend/home/lib/blocs/home_events.dart
+++ chap06/frontend/home/lib/blocs/home_events.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 abstract class HomeEvent extends Equatable {
   const HomeEvent();
@@ -20,4 +21,32 @@ class HomeLoadMoreEvent extends HomeEvent {
   const HomeLoadMoreEvent();
   @override
   List<Object> get props => [];
+}
+
+/// 切换底部导航栏
+class HomeSwitchBottomNavigationEvent extends HomeEvent {
+  final int index;
+
+  const HomeSwitchBottomNavigationEvent(this.index);
+
+  @override
+  List<Object> get props => [index];
+}
+
+/// 打开抽屉
+class HomeOpenEndDrawerEvent extends HomeEvent {
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  const HomeOpenEndDrawerEvent({required this.scaffoldKey});
+
+  @override
+  List<Object> get props => [scaffoldKey];
+}
+
+/// 关闭抽屉
+class HomeCloseEndDrawerEvent extends HomeEvent {
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  const HomeCloseEndDrawerEvent({required this.scaffoldKey});
+
+  @override
+  List<Object> get props => [scaffoldKey];
 }
--- chap06/frontend/home/lib/widgets/widgets.dart
+++ chap06/frontend/home/lib/widgets/widgets.dart
@@ -1,2 +1,8 @@
+export 'bottom_bar.dart';
+export 'left_drawer.dart';
 export 'load_more_widget.dart';
+export 'right_drawer.dart';
+export 'scaffold_body_widget.dart';
+export 'search_field.dart';
+export 'sliver_app_bar.dart';
 export 'sliver_list_body_widget.dart';
