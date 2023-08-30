e543d035ae79ea2dfcc74bc7074d4be1acef2ad6
Peng <wpcfan@gmail.com>
2023-08-30 16:57:11 +0800

    7-2 查询的种类以及返回类型的选择

--- chap07/backend/src/main/java/com/mooc/backend/repositories/PageLayoutRepository.java
+++ chap07/backend/src/main/java/com/mooc/backend/repositories/PageLayoutRepository.java
@@ -8,8 +8,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface PageLayoutRepository extends JpaRepository<PageLayout, Long> {
+    long countByStatus(PageStatus status);
+
+    Optional<PageLayout> findFirstByStatus(PageStatus status);
+
+    boolean existsByTitleContainingAllIgnoreCase(String title);
+
+    Stream<PageLayout> streamByPlatformAndPageTypeAndStatusAndStartTimeBeforeAndEndTimeAfter(Platform platform, PageType pageType, PageStatus status, LocalDateTime startTime, LocalDateTime endTime);
+
     List<PageLayout> findTop2ByPlatform(Platform platform);
     List<PageLayout> findByPlatformAndPageTypeAndStatusAndStartTimeBeforeAndEndTimeAfter(
             Platform platform,
--- chap07/backend/src/main/java/com/mooc/backend/rest/admin/PlaygroundController.java
+++ chap07/backend/src/main/java/com/mooc/backend/rest/admin/PlaygroundController.java
@@ -1,10 +1,13 @@
 package com.mooc.backend.rest.admin;
 
 import com.mooc.backend.entities.PageLayout;
+import com.mooc.backend.enumerations.PageStatus;
+import com.mooc.backend.enumerations.PageType;
 import com.mooc.backend.enumerations.Platform;
 import com.mooc.backend.repositories.PageLayoutRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Tag(name = "JPA 查询训练场", description = "查询相关接口")
 @Validated
@@ -24,5 +28,34 @@ public class PlaygroundController {
     @GetMapping("/top")
     public List<PageLayout> getTop2(@RequestParam Platform platform) {
         return pageLayoutRepository.findTop2ByPlatform(platform);
+    }
+
+    @GetMapping("/count")
+    public long count(@RequestParam PageStatus status) {
+        return pageLayoutRepository.countByStatus(status);
+    }
+
+    @GetMapping("/exists")
+    public boolean exists(@RequestParam String title) {
+        return pageLayoutRepository.existsByTitleContainingAllIgnoreCase(title);
+    }
+
+    @GetMapping("/first")
+    public PageLayout first(@RequestParam PageStatus status) {
+        return pageLayoutRepository.findFirstByStatus(status).orElseThrow();
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/stream")
+    public Stream<PageLayout> stream(@RequestParam Platform platform,
+                                     @RequestParam PageType pageType) {
+        var now = java.time.LocalDateTime.now();
+        return pageLayoutRepository.streamByPlatformAndPageTypeAndStatusAndStartTimeBeforeAndEndTimeAfter(
+                platform,
+                pageType,
+                PageStatus.PUBLISHED,
+                now,
+                now
+        );
     }
 }
