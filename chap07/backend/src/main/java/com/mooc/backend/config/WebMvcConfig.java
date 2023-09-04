78d1c0b5a3b5353e66a443b1c92ae9d1cd34f55f
Peng <wpcfan@gmail.com>
2023-09-04 14:17:28 +0800

    7-18 默认分页参数以及全局设定默认值

--- chap07/backend/src/main/java/com/mooc/backend/config/WebMvcConfig.java
+++ chap07/backend/src/main/java/com/mooc/backend/config/WebMvcConfig.java
@@ -2,12 +2,31 @@ package com.mooc.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PageableHandlerMethodArgumentResolver pageableHandlerMethodArgumentResolver() {
+        var resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(PageRequest.of(0, 20, Sort.by("id")));
+        return resolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(pageableHandlerMethodArgumentResolver());
+    }
 
     /**
      * 开启方法级别的参数校验，即在方法上加 Jakarta Validation 注解
--- chap07/backend/src/main/java/com/mooc/backend/rest/app/ProductController.java
+++ chap07/backend/src/main/java/com/mooc/backend/rest/app/ProductController.java
@@ -2,18 +2,17 @@ package com.mooc.backend.rest.app;
 
 import com.mooc.backend.entities.ProductData;
 import com.mooc.backend.repositories.ProductRepository;
-import com.mooc.backend.rest.vm.PageWrapper;
 import com.mooc.backend.rest.vm.SliceWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "商品查询", description = "客户端商品查询相关接口")
 @RequestMapping("/api/v1/app/products")
@@ -26,22 +25,8 @@ public class ProductController {
     @GetMapping("/by-category/{categoryId}")
     public SliceWrapper<ProductData> findProductsByCategoryId(
             @PathVariable long categoryId,
-            @RequestParam int page,
-            @RequestParam int pageSize,
-            @RequestParam(required = false, name = "sort") List<String> strSort
+            @ParameterObject Pageable pageable
             ) {
-        Sort sort = strSort.stream()
-                .map(str -> {
-                    String[] arr = str.split(",");
-                    if (arr.length == 1) {
-                        return Sort.by(arr[0]);
-                    } else {
-                        return Sort.by(arr[0]).descending();
-                    }
-                })
-                .reduce(Sort::and)
-                .orElse(Sort.unsorted());
-        Pageable pageable = PageRequest.of(page, pageSize, sort);
         var result = productRepository.findByCategoriesId(categoryId, pageable).map(ProductData::from);
         return new SliceWrapper<>(result.getNumber(), result.getSize(), result.getContent());
     }
