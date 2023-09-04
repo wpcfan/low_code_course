b24f1370edaac937d2e0c58bbff2ae27e8577ee2
Peng <wpcfan@gmail.com>
2023-09-04 12:13:45 +0800

    7-17 自定义分页参数，构建 Pageable

--- chap07/backend/src/main/java/com/mooc/backend/rest/app/ProductController.java
+++ chap07/backend/src/main/java/com/mooc/backend/rest/app/ProductController.java
@@ -7,12 +7,13 @@ import com.mooc.backend.rest.vm.SliceWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "商品查询", description = "客户端商品查询相关接口")
 @RequestMapping("/api/v1/app/products")
@@ -23,7 +24,24 @@ public class ProductController {
 
     @Operation(summary = "分页获取商品列表", description = "客户端瀑布流分页获取商品列表")
     @GetMapping("/by-category/{categoryId}")
-    public SliceWrapper<ProductData> findProductsByCategoryId(@PathVariable long categoryId, Pageable pageable) {
+    public SliceWrapper<ProductData> findProductsByCategoryId(
+            @PathVariable long categoryId,
+            @RequestParam int page,
+            @RequestParam int pageSize,
+            @RequestParam(required = false, name = "sort") List<String> strSort
+            ) {
+        Sort sort = strSort.stream()
+                .map(str -> {
+                    String[] arr = str.split(",");
+                    if (arr.length == 1) {
+                        return Sort.by(arr[0]);
+                    } else {
+                        return Sort.by(arr[0]).descending();
+                    }
+                })
+                .reduce(Sort::and)
+                .orElse(Sort.unsorted());
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
         var result = productRepository.findByCategoriesId(categoryId, pageable).map(ProductData::from);
         return new SliceWrapper<>(result.getNumber(), result.getSize(), result.getContent());
     }
