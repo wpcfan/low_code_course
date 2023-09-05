16b8a1fdf03208d729f44179ea13556b1d11e960
Peng <wpcfan@gmail.com>
2023-09-05 12:21:23 +0800

    update

--- chap08/backend/src/main/java/com/mooc/backend/json/PriceSerializer.java
+++ chap08/backend/src/main/java/com/mooc/backend/json/PriceSerializer.java
@@ -6,13 +6,14 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Locale;
 
 public class PriceSerializer extends JsonSerializer<BigDecimal> {
 
     @Override
     public void serialize(BigDecimal value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        var rounded = value.setScale(2, BigDecimal.ROUND_HALF_UP);
+        var rounded = value.setScale(2, RoundingMode.HALF_UP);
         var locale = Locale.CHINA;
         var numberFormat = java.text.NumberFormat.getCurrencyInstance(locale);
         gen.writeString(numberFormat.format(rounded));
